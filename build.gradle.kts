74d43db44188fe825f723571533303da62a81b16
Alex Zenla <alex@fork.run>
2023-05-17 18:24:03 -0700

    Dpendency updates.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
   application
 
-  kotlin("jvm") version "1.8.10"
-  kotlin("plugin.serialization") version "1.8.10"
+  kotlin("jvm") version "1.8.21"
+  kotlin("plugin.serialization") version "1.8.21"
 
   id("com.github.johnrengelman.shadow") version "8.1.0"
-  id("org.graalvm.buildtools.native") version "0.9.20"
+  id("org.graalvm.buildtools.native") version "0.9.22"
 }
 
 repositories {
@@ -23,15 +23,15 @@ java {
 dependencies {
   implementation("org.jetbrains.kotlin:kotlin-bom")
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
-  implementation("com.charleskorn.kaml:kaml:0.52.0")
+  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+  implementation("com.charleskorn.kaml:kaml:0.53.0")
   implementation("com.github.ajalt.clikt:clikt:3.5.2")
 
-  implementation("io.ktor:ktor-client-core:2.2.4")
-  implementation("io.ktor:ktor-client-cio:2.2.4")
+  implementation("io.ktor:ktor-client-core:2.3.0")
+  implementation("io.ktor:ktor-client-cio:2.3.0")
 
   implementation("com.zaxxer:nuprocess:2.0.6")
-  implementation("org.apache.commons:commons-compress:1.22")
+  implementation("org.apache.commons:commons-compress:1.23.0")
   implementation("me.tongfei:progressbar:0.9.5")
 
   implementation("net.swiftzer.semver:semver:1.2.0")
@@ -42,7 +42,7 @@ tasks.withType<KotlinCompile> {
 }
 
 tasks.withType<Wrapper> {
-  gradleVersion = "7.6"
+  gradleVersion = "8.1.1"
 }
 
 application {
